6c0e4e95d6fc19c83ddf16e1c65499b01b7ed910
eudiogolobo <diogolobo444@gmail.com>
2024-11-25 11:00:19 -0300

    first commit

--- config/db.go
+++ config/db.go
@@ -5,11 +5,30 @@ import (
     "fmt"
     "time"
     _ "github.com/go-sql-driver/mysql" // Importando o driver MySQL
+    "github.com/joho/godotenv"
+    "log"
+    "os"                               // Adicionando a importação do pacote os
+
 )
 
 // Função para conectar ao banco de dados MySQL
 func ConnectToDatabase() (*sql.DB, error) {
-    dsn := "root:@tcp(localhost:3306)/satnew" // Substitua pelas suas credenciais
+
+      // Carregar as variáveis de ambiente do arquivo .env
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Erro ao carregar arquivo .env: %v", err)
+	}
+
+    // Acessando variáveis de ambiente
+	databaseUrl := os.Getenv("DATABASE_URL")
+
+	if databaseUrl == "" {
+		// Valor padrão caso a variável de ambiente não esteja definida
+		databaseUrl = "root:@tcp(localhost:3306)/satnew"
+	}
+
+    dsn := databaseUrl // Substitua pelas suas credenciais
     db, err := sql.Open("mysql", dsn)
     if err != nil {
         return nil, fmt.Errorf("erro ao conectar ao banco de dados: %v", err)
